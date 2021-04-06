55ea72743f55daff51f817308781822971345c6f
Katerina Shevchenko <katyusha.shev18@gmail.com>
2021-04-06 18:07:49 +0300

    email and password for registration

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,10 @@ import Filter from "./filter.js";
 import {Button, Input, Select, Textarea, InputFieldTitle} from "./classes.js";
 
 
+// email: katya@katya.com
+// password: katyakatya
+
+
 function createContent() {
     const header = document.createElement("header");
     header.className = "header"
